bb68aa6579b943907e29f288e589c5a66b74366a
Dillon Wilson <greasyRooster1@gmail.com>
2026-03-17 13:57:59 -0700

    bit stuff

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,23 +1,32 @@
-
-struct InstructionArgs{
+struct InstructionArgs {
     data: u32,
 }
 
-impl InstructionArgs{
-    fn opcode(){
-    
+impl InstructionArgs {
+
+    fn from_bytes(bytes:Vec<u8>){
+        let data = 0;
+        for i in 0..bytes.len(){
+            data += bytes[i]<<(i*8-24)``
+        }
+    }
+
+    fn opcode(&self) -> u8 {
+        (self.data >> 28) as u8
     }
 }
 
-trait Instruction{
-    fn execute(InstructionArgs args);
+trait Instruction {
+    fn execute(args: InstructionArgs);
 
     fn bytes_len() -> i32;
 
     fn opcode() -> u8;
 }
 
-struct ADD{
-    fn execute(InstructionArgs args)
+struct ADD;
+
+impl Instruction for ADD {
+    fn execute(args: InstructionArgs) {}
 }
 
