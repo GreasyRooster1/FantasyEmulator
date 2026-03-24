d9e12c7558f473e6e50849fc0c4c271b0de1f45a
Dillon Wilson <greasyRooster1@gmail.com>
2026-03-24 13:36:22 -0700

    fix

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use bevy::prelude::*;
 const MEM_SIZE: usize = 65_535;
 
 #[derive(Resource)]
-struct Emulator{
+struct Emulator {
     physical_memory: [u8; MEM_SIZE],
     registers: Registers,
 }
 
-struct Registers{
+struct Registers {
     r0: u8,
 
     r1: u8,
@@ -18,21 +18,19 @@ struct Registers{
     r3: u8,
     r4: u8,
     r5: u8,
-    r6: u8
-    r7: u8
-    r8: u8
-    r9: u8
-    r10: u8
-    r11: u8
-    r12: u8
-    r13: u8
+    r6: u8,
+    r7: u8,
+    r8: u8,
+    r9: u8,
+    r10: u8,
+    r11: u8,
+    r12: u8,
+    r13: u8,
 
-    program_counter:u8,
+    program_counter: u8,
     return_address: u8,
 }
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .run();
+    App::new().add_plugins(DefaultPlugins).run();
 }
