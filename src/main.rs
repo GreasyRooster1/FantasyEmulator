1c713f4dd79d84de3215f1d94cabcbec491f0638
Dillon Wilson <greasyRooster1@gmail.com>
2026-03-24 13:36:06 -0700

    more registers

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,14 @@ struct Registers{
     r3: u8,
     r4: u8,
     r5: u8,
+    r6: u8
+    r7: u8
+    r8: u8
+    r9: u8
+    r10: u8
+    r11: u8
+    r12: u8
+    r13: u8
 
     program_counter:u8,
     return_address: u8,
