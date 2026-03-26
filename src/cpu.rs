65327d0ce27c5c7f7468b5f3fb1d5a8e136ad4c5
Dillon Wilson <greasyRooster1@gmail.com>
2026-03-26 12:41:04 -0700

    fix rom loading

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,6 +1,6 @@
-use std::fs;
+use crate::{MEM_SIZE, PC_REGISTER, REGISTER_COUNT, ROM_SIZE, get_nibble_from_byte};
 use bevy::prelude::Resource;
-use crate::{get_nibble_from_byte, MEM_SIZE, PC_REGISTER, REGISTER_COUNT, ROM_SIZE};
+use std::fs;
 
 #[derive(Resource)]
 pub struct Emulator {
@@ -9,8 +9,7 @@ pub struct Emulator {
     rom_disk: [u8; ROM_SIZE],
 }
 
-impl Emulator{
-
+impl Emulator {
     pub fn hardware_setup() -> Emulator {
         Emulator {
             physical_memory: [0; MEM_SIZE],
@@ -19,12 +18,12 @@ impl Emulator{
         }
     }
 
-    pub fn boot(&mut self,disk_path:String){
+    pub fn boot(&mut self, disk_path: String) {
         self.install_rom_disk(disk_path);
-        self.load_rom_to_memory(0x0000,self.rom_disk.len() as u16);
+        self.load_rom_to_memory(0x0000, self.rom_disk.len() as u16);
     }
 
-    pub fn cpu_cycle(&mut self){
+    pub fn cpu_cycle(&mut self) {
         let pc_memory_value = self.physical_memory[self.registers[PC_REGISTER] as usize];
         let opcode_nibble = get_nibble_from_byte(pc_memory_value as u32, 0);
         println!("{:#b}", opcode_nibble);
@@ -41,9 +40,12 @@ impl Emulator{
     pub fn load_rom_to_memory(&mut self, start_address: u16, amount: u16) {
         let start = start_address as usize;
         let end = start + amount as usize;
+        let mut rom_idx = 0;
         for i in start..end {
-            let byte = self.rom_disk[i];
-            self.rom_disk[i] = byte;
+            let byte = self.rom_disk[rom_idx];
+            self.physical_memory[i] = byte;
+            rom_idx += 1;
         }
     }
 }
+
