4056d5517cba54cc4862c403ea04808d20f10a72
GreasyRooster1 <greasyRooster1@gmail.com>
2026-03-25 20:25:54 -0700

    reg count

--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,15 @@ mod tests;
 
 use bevy::prelude::*;
 
-const MEM_SIZE: usize = 65_535;
+const MEM_SIZE: usize = 65_535; //limited by 8bit bytes
+const REGISTER_COUNT: usize = 16; // limited by asm arguments
 
 #[derive(Resource)]
 struct Emulator {
     physical_memory: [u8; MEM_SIZE],
-    registers: Registers,
+    registers: [u8; MEM_SIZE],
 }
 
-struct Registers {
-    r0: u8,
-
-    r1: u8,
-    r2: u8,
-    r3: u8,
-    r4: u8,
-    r5: u8,
-    r6: u8,
-    r7: u8,
-    r8: u8,
-    r9: u8,
-    r10: u8,
-    r11: u8,
-    r12: u8,
-    r13: u8,
-
-    program_counter: u8,
-    return_address: u8,
-}
 
 fn main() {
     App::new().add_plugins(DefaultPlugins).run();
