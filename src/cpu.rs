0a79ab0a7472dc6bd1a9f08ccc66de113b5878a5
GreasyRooster1 <greasyRooster1@gmail.com>
2026-03-25 20:29:49 -0700

    cpu

---
+++ src/cpu.rs
@@ -0,0 +1,14 @@
+use bevy::prelude::Resource;
+use crate::{MEM_SIZE, REGISTER_COUNT};
+
+#[derive(Resource)]
+struct Emulator {
+    physical_memory: [u8; MEM_SIZE],
+    registers: [u8; REGISTER_COUNT],
+}
+
+impl Emulator{
+    pub fn cpu_cycle(&mut self){
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 mod instructions;
 mod tests;
+mod cpu;
 
 use bevy::prelude::*;
 
 const MEM_SIZE: usize = 65_535; //limited by 8bit bytes
 const REGISTER_COUNT: usize = 16; // limited by asm arguments
-
-#[derive(Resource)]
-struct Emulator {
-    physical_memory: [u8; MEM_SIZE],
-    registers: [u8; REGISTER_COUNT],
-}
 
 
 fn main() {
