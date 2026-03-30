fda71a55429b90d846242f8bc8d2870a1f677bb1
GreasyRooster1 <greasyRooster1@gmail.com>
2026-03-30 16:51:51 -0700

    screen stuff

--- assets/screen.wgsl
+++ assets/screen.wgsl
@@ -1,8 +1,15 @@
 #import bevy_sprite::mesh2d_vertex_output::VertexOutput
 
+struct ScreenData {
+  data: array<u32, 2048>
+};
+
 @group(#{MATERIAL_BIND_GROUP}) @binding(0) var<uniform> material_color: vec4<f32>;
+@group(#{MATERIAL_BIND_GROUP}) @binding(1) var<uniform> data: ScreenData;
 
 @fragment
 fn fragment(mesh: VertexOutput) -> @location(0) vec4<f32> {
-    return vec4(1.0,1.0,1.0,1.0);
+    let uv = mesh.uv;
+    let pixel_pos = floor(uv*128.0)/128.0;
+    return vec4(pixel_pos,1.0,1.0);
 }
--- src/screen.rs
+++ src/screen.rs
@@ -3,16 +3,27 @@ use bevy::color::LinearRgba;
 use bevy::image::Image;
 use bevy::math::{Vec3, Vec4};
 use bevy::mesh::{Mesh, Mesh2d};
-use bevy::prelude::{Camera2d, Commands, MeshMaterial2d, Rectangle, Res, ResMut, Transform, TypePath};
-use bevy::render::render_resource::AsBindGroup;
+use bevy::prelude::{Camera2d, Commands, Component, MeshMaterial2d, Query, Rectangle, Res, ResMut, Transform, TypePath, With};
+use bevy::render::render_resource::{AsBindGroup, Buffer, ShaderType};
 use bevy::shader::ShaderRef;
 use bevy::sprite_render::Material2d;
+
+
+#[derive(Clone, Debug, ShaderType)]
+pub struct ScreenData{
+    data:[u32;2048],
+}
 
 #[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
 pub struct ScreenMaterial {
     #[uniform(0)]
     color: LinearRgba,
+    #[uniform(1)]
+    screen_data: ScreenData,
 }
+
+#[derive(Component)]
+struct Screen;
 
 impl Material2d for ScreenMaterial {
     fn fragment_shader() -> ShaderRef {
@@ -34,8 +45,20 @@ pub fn setup_screen(
         Mesh2d(meshes.add(Rectangle::default())),
         MeshMaterial2d(materials.add(ScreenMaterial {
             color: LinearRgba::BLUE,
-
+            screen_data: ScreenData{data:[0; 2048]},
         })),
-        Transform::default().with_scale(Vec3::splat(128.)),
+        Transform::default().with_scale(Vec3::splat(500.)),
+        Screen
     ));
+}
+
+fn update_screen(
+    mut materials: ResMut<Assets<ScreenMaterial>>,
+    query: Query<&mut MeshMaterial2d<ScreenMaterial>, With<Screen>>,
+) {
+    for handle in query.iter() {
+        if let Some(material) = materials.get_mut(handle) {
+            material.screen_data.data=[1; 2048]
+        }
+    }
 }
