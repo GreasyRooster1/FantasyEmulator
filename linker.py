5ec294e2eca2f3f48ea8486982a0db0cb822b324
Dillon Wilson <greasyRooster1@gmail.com>
2026-04-02 13:49:19 -0700

    opcodes

--- linker.py
+++ linker.py
@@ -1,5 +1,22 @@
 
-
+opcodes = {
+    "NOP": 0,
+    "ADD": 1,
+    "SUB": 2,
+    "MUL": 3,
+    "DIV": 4,
+    "INC": 5,
+    "REM": 6,
+    "NOT": 7,
+    "AND": 8,
+    "OR": 9,
+    "XOR": 10,
+    "PEEK": 11,
+    "POKE": 12,
+    "LODI": 13,
+    "BRANCH": 14,
+    "HALT": 15,
+}
 
 machine_code = []
 
