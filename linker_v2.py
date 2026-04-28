615af76a8deb17a89bdeec8794ac2d3a94cdf44f
Dillon Wilson <greasyRooster1@gmail.com>
2026-04-29 02:48:33 +0700

    tags

--- linker_v2.py
+++ linker_v2.py
@@ -53,49 +53,53 @@ opcodes = {
 
 }
 
-branch_ops = [
-    "jump",
-    "==",
-    "!=",
-    ">",
-    ">=",
-    "<",
-    "<="
-]
 
 machine_code = []
+tags = {}
 
 
+def do_instruct(line):
+    words = line.replace("_"," ").split()
+    opcode = opcodes[words[0]]
+    args = []
+    words.pop(0)
+    for word in words:
+        word = word.replace("r","",1)
+        if word.startswith("0x"):
+            args.append(int(word[2:],16))
+        elif word.startswith("b"):
+            args.append(int(word[1:],2))
+        elif word.startswith("."):
+            args.append(tags[word])
+        else:
+            args.append(int(word))
+
+    machine_code.append(opcode)
+
+    for arg in args:
+        machine_code.append(arg)
+
+def do_tag(line)
+    tags[line] = address
+
+address = 0
 with open('programs/'+sys.argv[1]+'.asm', 'r') as file:
     for line in file:
         if line.startswith(";") or len(line.strip())<1:
             continue
+
         print(line.strip())
-        for i in range(len(branch_ops)):
-            line = line.replace(branch_ops[i],str(i))
-        words = line.replace("_"," ").split()
-        opcode = opcodes[words[0]]
-        args = []
-        words.pop(0)
-        for word in words:
-            word = word.replace("r","",1)
-            if word.startswith("0x"):
-                args.append(int(word[2:],16))
-            elif word.startswith("b"):
-                args.append(int(word[1:],2))
-            else:
-                args.append(int(word))
-        machine_code.append(opcode)
-        for arg in args:
-            machine_code.append(arg)
 
-compressed = []
-for i in range(0,len(machine_code),2):
-    compressed.append(machine_code[i+1]+(machine_code[i]<<4))
+        if line.startswith("."):
+            do_tag(line)
+        else:
+            do_instruct(line)
+        address+=1
 
-byte_data = bytes(compressed)
 
-with open('rom.rom', 'wb') as f:
+byte_data = bytes(machine_code)
+
+with open(sys.argv[1]+'.rom', 'wb') as f:
     f.write(byte_data)
 
 print(machine_code)
---
+++ programs/bouncing_ball_v2.asm
@@ -0,0 +1,25 @@
+; r0 = 1
+; r1 = ballx
+; r2 = bally
+; r3 = ballx dir
+; r4 = bally dir
+; r5 = screen w
+; r6 = screen h
+
+LODI r0 0_0_0_1
+
+; bounce x  0100
+BRANCH r3 == r0 1 12
+BRANCH r3 != r0 1 16
+; 12
+LODI r3 0_1
+; 16
+LODI r3 0_0
+
+; bounce y 0200
+BRANCH r4 == r0 2 12
+BRANCH r4 != r0 2 16
+; 12
+LODI r4 0_1
+; 16
+LODI r4 0_0
